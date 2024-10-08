19779c9a76daf8020713a28bf66f83f8dc0237fe
Brian Keeley <imbriankeeley@gmail.com>
2024-10-08 15:56:17 -0400

    Created fetch for weather and made function that will return an object with the data from this fetch

---
+++ main.js
@@ -0,0 +1,33 @@
+
+const exampleCity = 'london'
+
+async function getWeather(city) {
+    if (city) {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=E7EJGZH6DBLK6X57E9TVEJ8QG`, {mode: 'cors'});
+        const location = await response.json();
+        
+        const address = location.address;
+        const conditions = location.currentConditions.conditions;
+        const temp = JSON.stringify(location.currentConditions.temp);
+        const humidity = JSON.stringify(location.currentConditions.humidity);
+        const feelslike = JSON.stringify(location.currentConditions.feelslike);
+        const windspeed = JSON.stringify(location.currentConditions.windspeed);
+        
+        return objectifyWeather(address, conditions, temp, humidity, feelslike, windspeed);        
+
+}}
+    
+function objectifyWeather(address, conditions, temp, humidity, feelslike, windspeed) {
+    const weather = {
+        address: address,
+        condition: conditions,
+        temp: temp,
+        humidity: humidity,
+        feelslike: feelslike,
+        windspeed: windspeed
+    }
+
+    return weather;
+}
+
+getWeather(exampleCity);
