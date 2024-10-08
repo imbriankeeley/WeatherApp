324993bf17cfd55ff13e676f029f36897e38cd9e
Brian Keeley <imbriankeeley@gmail.com>
2024-10-08 19:05:06 -0400

    Added dynamic changes to location and conditions when city is fethed and added a loading sign

--- main.js
+++ main.js
@@ -1,13 +1,74 @@
+const conditionIcon = document.getElementById('condition-icon');
+const feelslikeIcon = document.getElementById('feelslike-icon');
+const fToCButton = document.getElementById('f-c');
 
+const fToCSpan = document.querySelectorAll('.f-c-span');
+
+const locationValue = document.getElementById('location');
+const conditionsValue = document.getElementById('conditions');
+const temperatureValue = document.getElementById('temperature');
+const humidityValue = document.getElementById('humidity');
+const feelslikeValue = document.getElementById('feels-like');
+const windspeedValue = document.getElementById('wind-speed');
+
+const realTemp = document.getElementById('real-temp');
+const feelslikeTemp = document.getElementById('feels-like-temp');
+
+const form = document.getElementById('weather-form');
+const cityInput = document.getElementById('city-input');
+
+const pickCity = document.getElementById('pick-city');
+const weatherInfo = document.getElementById('weather-info');
+
+const loader = document.getElementById('loader');
+
+function celsToFar(num) {
+    const cels = Number(num);
+    const result = Math.round((9/5) * cels + 32);
+    return result.toString();
+}
+
+function farToCels(num) {
+    const far = Number(num) - 32;
+    const result = Math.round(far * (5/9));
+    return result.toString();
+}
+
+fToCButton.addEventListener('click', () => {
+    const text = fToCButton.innerText;
+    if (text == 'Change to Fahrenheit') {
+        fToCButton.innerText = 'Change to Celsius';
+        for (const span of fToCSpan) {
+            span.innerText = '°F'
+        }
+
+        realTemp.innerText = celsToFar(realTemp.innerText);
+        feelslikeTemp.innerText = celsToFar(feelslikeTemp.innerText);
+
+    }
+
+    if (text == 'Change to Celsius') {
+        fToCButton.innerText = 'Change to Fahrenheit';
+        for (const span of fToCSpan) {
+            span.innerText = '°C'
+        }
+
+        realTemp.innerText = farToCels(realTemp.innerText);
+        feelslikeTemp.innerText = farToCels(feelslikeTemp.innerText);
+
+    }
+});
 
 
 async function getWeather(city) {
     if (city) {
+        loader.classList.remove('hidden');
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=E7EJGZH6DBLK6X57E9TVEJ8QG`, {mode: 'cors'});
+        loader.classList.add('hidden');
         const location = await response.json();
         
-        const address = location.address;
-        const conditions = location.currentConditions.conditions;
+        const address = await location.address;
+        const conditions = await location.currentConditions.conditions;
         const temp = JSON.stringify(location.currentConditions.temp);
         const humidity = JSON.stringify(location.currentConditions.humidity);
         const feelslike = JSON.stringify(location.currentConditions.feelslike);
@@ -29,3 +90,49 @@ function objectifyWeather(address, conditions, temp, humidity, feelslike, windsp
 
     return weather;
 }
+
+function populateWeatherApp(address, condition) {
+    if (!pickCity.classList.contains('hidden')) {
+        pickCity.classList.add('hidden');
+    }
+
+    if (weatherInfo.classList.contains('hidden')) {
+        weatherInfo.classList.remove('hidden');
+    }
+
+    locationValue.innerText = address;
+    conditionsValue.innerText = condition;
+}
+
+// const locationValue = document.getElementById('location');
+// const conditionsValue = document.getElementById('conditions');
+// const temperatureValue = document.getElementById('temperature');
+// const humidityValue = document.getElementById('humidity');
+// const feelslikeValue = document.getElementById('feels-like');
+// const windspeedValue = document.getElementById('wind-speed');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value.trim();
+
+    const weather = await getWeather(city);
+
+    let location = weather.address.toLowerCase();
+    const nameArray = location.split(' ');
+    let address = '';
+    nameArray.forEach(element => {
+        address += element.charAt(0).toUpperCase() + element.slice(1) + ' ';
+    });
+
+    let conditions = weather.condition.toLowerCase();
+    const conditionArray = conditions.split(' ');
+    let condition = '';
+    conditionArray.forEach(element => {
+        condition += element.charAt(0).toUpperCase() + element.slice(1) + ' ';
+    })
+    
+
+    populateWeatherApp(address.trim(), condition.trim(), )
+
+});
