c500704277694441bdef2e7b074dfb66f746d04d
Brian Keeley <imbriankeeley@gmail.com>
2024-10-08 21:56:18 -0400

    Refactored sizing to prevent zoom when typing on mobile

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+if(navigator.userAgent.indexOf('iPhone') > -1 )
+    {
+        document
+          .querySelector("[name=viewport]")
+          .setAttribute("content","width=device-width, initial-scale=1, maximum-scale=1");
+    }
+
+
 const conditionIcon = document.getElementById('condition-icon');
 const fToCButton = document.getElementById('f-c');
 
